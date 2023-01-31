a5905279484591e3f8c2c9a4fad550187b68e364
bonzzy <tomislav.fabeta@gmail.com>
2023-01-31 09:54:13 +0100

    reading port from PORT env

--- setup/setup_router.go
+++ setup/setup_router.go
@@ -5,14 +5,20 @@ import (
 	"github.com/bonzzy/teltech-go-challenge/core"
 	"github.com/bonzzy/teltech-go-challenge/dtos"
 	"github.com/bonzzy/teltech-go-challenge/middleware"
+	"os"
+	"strconv"
 )
 
 func RouterSetup() core.TinyGinServer {
-	tinyGinServer := core.NewTinyGin(8000)
+	portEnv := os.Getenv("PORT")
+	port, _ := strconv.Atoi(portEnv)
+
+	tinyGinServer := core.NewTinyGin(port)
 	tinyGinServer.Get("/healthz", controllers.Healthz)
 	tinyGinServer.Get("/add", middleware.CacheWrapper(controllers.Add, dtos.Response{}))
 	tinyGinServer.Get("/subtract", middleware.CacheWrapper(controllers.Subtract, dtos.Response{}))
 	tinyGinServer.Get("/multiply", middleware.CacheWrapper(controllers.Multiply, dtos.Response{}))
 	tinyGinServer.Get("/divide", middleware.CacheWrapper(controllers.Divide, dtos.Response{}))
+
 	return tinyGinServer
 }
