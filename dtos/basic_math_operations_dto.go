58771f661383bf2b9e1e79d82a27ceee5ee6593c
bonzzy <tomislav.fabeta@gmail.com>
2023-02-01 18:53:12 +0100

    cleaned code

--- dtos/basic_math_operations_dto.go
+++ dtos/basic_math_operations_dto.go
@@ -1,12 +1,5 @@
 package dtos
 
-import "math/big"
-
-type MathParams struct {
-	X *big.Float `json:"x" form:"x" binding:"required,number"`
-	Y *big.Float `json:"y" form:"y" binding:"required,number"`
-}
-
 type Response struct {
 	Action string  `json:"action"`
 	X      float64 `json:"x"`
